eeb214357384c4e6886e118ce5874c49948ae25e
zainirshad1 <zain.pate@gmail.com>
2025-08-11 01:41:41 +0530

    Create handleLogin.js

---
+++ public/js/Namaztimes/auth/handleLogin.js
@@ -0,0 +1,8 @@
+// public/js/Namaztimes/auth/handleLogin.js
+
+/**
+ * Handles the login action by redirecting the user to the login page.
+ */
+export function handleLogin() {
+    window.location.href = 'login.html';
+}
