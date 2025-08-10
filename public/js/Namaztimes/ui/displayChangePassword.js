9a19f4709c50720c9450f132356a1d0ac1346706
zainirshad1 <zain.pate@gmail.com>
2025-08-11 01:45:17 +0530

    Create displayChangePassword.js

---
+++ public/js/Namaztimes/ui/displayChangePassword.js
@@ -0,0 +1,9 @@
+// public/js/Namaztimes/ui/displayChangePassword.js
+
+/**
+ * Shows the change password modal.
+ * @param {HTMLElement} changePasswordModalOverlay - The modal overlay element.
+ */
+export function displayChangePassword(changePasswordModalOverlay) {
+    changePasswordModalOverlay.classList.add('show');
+}
